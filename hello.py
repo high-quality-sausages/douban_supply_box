3759e29ed385c5c52e2bc02f590983db1a5056c9
bee <Bee0_0@icloud.com>
2020-02-12 23:46:51 +0800

    first init :)

---
+++ hello.py
@@ -0,0 +1,13 @@
+def main():
+    for _ in range(50):
+        print("-", end="")
+    print()
+    for _ in range(7):
+        print(" ", end="")
+    print("这是一个供优选香肠调用的补给箱📦")
+    for _ in range(50):
+        print("-", end="")
+
+
+if __name__ == "__main__":
+    main()
